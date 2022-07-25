0428cee94d85462497891c073785df17d44191b7
Cindy Vuong <vuongcindytv@gmail.com>
2022-07-25 15:08:35 -0700

    Adds user auth frontend routes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,10 +1,9 @@
 // src/components/app.js
 
 import React from 'react';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { AuthRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
