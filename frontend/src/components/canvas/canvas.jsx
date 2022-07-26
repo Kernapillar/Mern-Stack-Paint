31f848b71f6334537bf8d5fa7a4c297f0c2d0d48
Alexander Kern <alexander.t.kern@gmail.com>
2022-07-26 09:24:32 -0700

    canvas clear and color buttons

--- frontend/src/components/canvas/canvas.jsx
+++ frontend/src/components/canvas/canvas.jsx
@@ -9,9 +9,13 @@ function CanvasComponent() {
     const [lineColor, setLineColor] = useState("black");
     const [drawSize, setDrawSize] = useState(5);
 
+    const history = []
+
+
     useEffect(() => {
         const canvas = canvasElement.current;
         const ctx = canvas.getContext("2d");
+        
         ctx.lineCap = "round";
         ctx.lineJoin = "round";
         ctx.globalAlpha = 1;
@@ -20,8 +24,13 @@ function CanvasComponent() {
         canvasContext.current = ctx;
     }, [lineColor, drawSize]);
 
+    const clearCanvas = () =>  {
+        canvasContext.current.fillStyle = "white"
+        canvasContext.current.clearRect(0, 0, canvasElement.current.width, canvasElement.current.height)
+        canvasContext.current.fillRect(0, 0, canvasElement.current.width, canvasElement.current.height)
+    }
 
-    const beginDrawing = (e) => {
+    const start = (e) => {
         canvasContext.current.beginPath();
         canvasContext.current.moveTo(
             e.nativeEvent.offsetX,
@@ -30,7 +39,7 @@ function CanvasComponent() {
         setIsDrawing(true)
     }
 
-    const stopDrawing = (e) => {
+    const stop = (e) => {
         canvasContext.current.closePath();
         setIsDrawing(false);
     }
@@ -48,10 +57,10 @@ function CanvasComponent() {
 
     return (
         <div className="canvas-element">
-            <Menu className="canvas-menu" setLineColor={setLineColor} setDrawSize={setDrawSize}/>
+            <Menu className="canvas-menu" setLineColor={setLineColor} setDrawSize={setDrawSize} clearCanvas={clearCanvas}/>
             <canvas className="draw-area"
-            onMouseDown={beginDrawing} 
-            onMouseUp={stopDrawing} 
+            onMouseDown={start} 
+            onMouseUp={stop} 
             onMouseMove={draw}
             ref={canvasElement}
             width={'1000px'}
