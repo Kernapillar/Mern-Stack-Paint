ef56e5ee4989063ca2eadb77f673cd348b56730f
Alexander Kern <alexander.t.kern@gmail.com>
2022-07-28 15:17:12 -0700

    basic index post card styling

--- frontend/src/components/posts/posts_single.jsx
+++ frontend/src/components/posts/posts_single.jsx
@@ -5,17 +5,22 @@ class PostSingle extends React.Component {
   render() {
     return (
       <div className="post-container">
-        <h3>Title</h3>
-        <img src="https://kernify-seed.s3.us-west-1.amazonaws.com/wish_you_were_here.jpg" alt="post-picture" className='post-card-pic'/>
-        <div className="tags">{this.props.tag}</div>
-        <p className="post">{this.props.text}</p>
-        {console.log(this.props)}
+        <div className='single-title'>
+          <h3 className='post-card-title'>Title</h3>
+          <h3>{" "}</h3>
+        </div>
+        <div className='picture-background'>
+          <img src="https://mernstackdrawwizard.s3.us-west-1.amazonaws.com/1659028971443" alt="post-picture" className='post-card-pic'/>
+        </div>
+        
+        {/* <p className="post">{this.props.text}</p> */}
+        {/* {console.log(this.props)} */}
        {/* <button className="post-reply-button">reply to this post</button> */}
         {/* <h3>{this.props.imageurl}</h3> */}
         <hr />
         <div className='comments-tags'>
           <p>x comments</p>
-          <p>{this.props.tag} tag here</p>
+          <div className="tags">{this.props.tag}</div>
         </div>
       </div>
     );
