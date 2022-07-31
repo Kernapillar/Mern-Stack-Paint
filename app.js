6301c5c91cbd172a7f0a3d7cd7907c8efb5367f0
Alexander Kern <alexander.t.kern@gmail.com>
2022-07-31 12:11:29 -0700

    Heroku setup

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const posts = require('./routes/api/posts')
 const User = require('./models/User')
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
 .connect(db, { useNewUrlParser: true })
